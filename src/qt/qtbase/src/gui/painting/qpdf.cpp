8c5159897897d8691de60fa3375ca97b7ae71cf1
Stephen Compall <scompall@spcapitaliq.com>
2013-09-12 11:26:45 -0400

    Obey absolute letterSpacing when writing PDF.
    * src/qt/qtbase/src/gui/painting/qpdf.cpp
      (QPdfEnginePrivate::drawTextItem):
    
      Positions of each glyph did not take into account the letterSpacing
      property of the text item's QFont.  Here we adjust using the
      absolute spacing, if it is present, via a not-so-pleasant method.
    
      This makes absolute settings of the CSS letter-spacing property
      actually apply in non-SVG contexts when writing PDF.  Spacing is
      already handled correctly for SVG, because qpdf isn't responsible
      for layout of individual glyphs in that case.
    
      Relative letter-spacing and any word-spacing are still not
      supported.

--- src/qt/qtbase/src/gui/painting/qpdf.cpp
+++ src/qt/qtbase/src/gui/painting/qpdf.cpp
@@ -2541,6 +2541,7 @@ void QPdfEnginePrivate::drawTextItem(const QPointF &p, const QTextItemInt &ti)
         return;
     int synthesized = ti.fontEngine->synthesized();
     qreal stretch = synthesized & QFontEngine::SynthesizedStretch ? ti.fontEngine->fontDef.stretch/100. : 1.;
+    qreal letterSpacing = ti.f->letterSpacingType() == QFont::AbsoluteSpacing ? ti.f->letterSpacing() : 0;
 
     *currentPage << "BT\n"
                  << "/F" << font->object_id << size << "Tf "
@@ -2579,6 +2580,7 @@ void QPdfEnginePrivate::drawTextItem(const QPointF &p, const QTextItemInt &ti)
         qreal y = positions[i].y.toReal();
         if (synthesized & QFontEngine::SynthesizedItalic)
             x += .3*y;
+        x += i*letterSpacing;
         x /= stretch;
         char buf[5];
         int g = font->addGlyph(glyphs[i]);
@@ -2599,6 +2601,7 @@ void QPdfEnginePrivate::drawTextItem(const QPointF &p, const QTextItemInt &ti)
             qreal y = positions[i].y.toReal();
             if (synthesized & QFontEngine::SynthesizedItalic)
                 x += .3*y;
+            x += i*letterSpacing;
             x /= stretch;
             char buf[5];
             int g = font->addGlyph(glyphs[i]);
